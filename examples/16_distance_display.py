f8e1e36be512d388e4db0e40c982c8c0ae20b674
GollumBree <daniel.wagner@gsglu.wwschool.de>
2025-07-01 10:56:15 +0200

    fix script 16

--- examples/16_distance_display.py
+++ examples/16_distance_display.py
@@ -1,57 +1,55 @@
 import time
-import socket,signal
-import RPi.GPIO as gpio
-import math
-import Adafruit_CharLCD as LCD
-import smbus
+import RPi.GPIO as GPIO
+import board
+import adafruit_character_lcd.character_lcd_i2c as LCD
 
 
+GPIO.setmode(GPIO.BCM)
 
 def dis():
-	trig = 15
-	echo = 14
-	gpio.setup(trig,gpio.OUT)
-	gpio.setup(echo,gpio.IN)
+    trig = 15
+    echo = 14
+    GPIO.setup(trig, GPIO.OUT)
+    GPIO.setup(echo, GPIO.IN)
 
-	gpio.output(trig,False)
-	time.sleep(0.5)
-	
-	gpio.output(trig,True)
-	time.sleep(0.00001)
-	gpio.output(trig,False)
+    GPIO.output(trig, False)
+    time.sleep(0.5)
 
-	while gpio.input(echo)==0:
-		pulse_start = time.time()
+    GPIO.output(trig, True)
+    time.sleep(0.00001)
+    GPIO.output(trig, False)
 
-	while gpio.input(echo)==1:
-		pulse_end = time.time()
+    while GPIO.input(echo) == 0:
+        pulse_start = time.time()
 
-	pulse_duration =  pulse_end - pulse_start
+    while GPIO.input(echo) == 1:
+        pulse_end = time.time()
 
-   	distance = pulse_duration * 17150
+    pulse_duration = pulse_end - pulse_start
 
-   	distance = round(distance, 2)
+    distance = pulse_duration * 17150
 
-	return distance
+    distance = round(distance, 2)
 
+    return distance
 
 
 # define LCD column and row size for 16x2 LCD
 lcd_columns = 16
 lcd_rows = 2
 # initialize the I2C address for LCD
-lcd = LCD.Adafruit_CharLCDBackpack(address = 0x20)
+lcd = LCD.Character_LCD_I2C(board.I2C(), lcd_columns, lcd_rows, address=0x20) #LCD.Adafruit_CharLCDBackpack(address=0x20)
+
 # turn backlight on
-lcd.set_backlight(0)
-	
+lcd.backlight = True
+
 try:
     while True:
-	lcd.message('{0:0.2f}cm'.format(dis()))
-	time.sleep(2)
-	lcd.clear()
+        lcd.message = "{0:0.2f}cm".format(dis())
+        time.sleep(1)
+        lcd.clear()
 
 except KeyboardInterrupt:
-	lcd.clear()
-        lcd.set_backlight(1)
-        gpio.cleanup()
-
+    lcd.clear()
+    lcd.backlight = False
+    GPIO.cleanup()
